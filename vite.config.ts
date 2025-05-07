859af6e2597b761414d2c106a35df3fb475afd61
Anis Sebiane <105225783+SebianeAnis@users.noreply.github.com>
2025-05-07 15:47:36 +0200

    correction build de vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: "https://github.com/SebianeAnis/githubpages-yanis",
+  build : {
+    outDir : 'dist'
+  },
   plugins: [react()],
 })
